155bcb78298dbae5f383ce48bb52c2719887f4f7
SEBASTIAO RODRIGO NASCIMENTO SOUSA <rodrigoexer1@gmail.com>
2025-02-02 14:57:52 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cors from 'cors';
 import { filterGeral } from './filter/filterGeral';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
   // Registrando seu filtro
-  app.use(cors());
+  app.enableCors({
+    origin: '*',
+  })
   app.useGlobalFilters(new filterGeral())
 
   await app.listen(process.env.PORT ?? 3001);
